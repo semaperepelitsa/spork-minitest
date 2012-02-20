38343533143b50e18887530e19368974d06fb976
Semyon Perepelitsa <sema@sema.in>
2012-02-20 17:05:28 +0800

    Add sample test directory

---
+++ test/second_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SecondTest < MiniTest::Unit::TestCase
+  def test_failure
+    flunk "Everything is ok!"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+require "spork"
+
+Spork.prefork do
+  require "minitest/unit"
+end
