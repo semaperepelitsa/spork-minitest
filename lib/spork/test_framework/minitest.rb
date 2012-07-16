ac59be957b53b4d39ae61936a696790ba2233240
Everton Ribeiro <nuxlli@gmail.com>
2012-07-16 16:58:32 -0300

    Added support for -r, -e and -I

--- lib/spork/test_framework/minitest.rb
+++ lib/spork/test_framework/minitest.rb
@@ -13,20 +13,43 @@ class Spork::TestFramework::MiniTest < Spork::TestFramework
     else
       ::MiniTest::Unit.output = stdout
     end
-
-    paths, opts = parse_options(argv)
-    paths.each do |path|
-      require path
+    
+    # MiniTest's test/unit does not support -I, -r, or -e
+    # Extract them and remove from arguments that are passed to testrb.
+    argv.each_with_index do |arg, idx|
+      if arg =~ /-I(.*)/
+        if $1 == ''
+          # Path is next argument.
+          include_path = argv[idx + 1]
+          argv[idx + 1] = nil # Will be squashed when compact called.
+        else
+          include_path = $1
+        end
+        $LOAD_PATH << include_path
+        argv[idx] = nil
+      elsif arg =~ /-r(.*)/
+        if $1 == ''
+          # File is next argument.
+          require_file = argv[idx + 1]
+          argv[idx + 1] = nil # Will be squashed when compact called.
+        else
+          require_file = $1
+        end
+        require require_file
+        argv[idx] = nil
+      elsif arg =~ /^-e$/
+        eval argv[idx + 1]
+        argv[idx] = argv[idx + 1] = nil
+      elsif arg == '--'
+        argv[idx] = nil
+        break
+      elsif !arg.nil?
+        require arg
+        argv[idx] = nil
+      end
     end
+    argv.compact!
 
-    ::MiniTest::Unit.new.run(opts)
-  end
-
-  def parse_options(argv)
-    paths, opts = argv.slice_before("--").to_a
-    paths ||= []
-    opts ||= []
-    opts.shift
-    [paths, opts]
+    ::MiniTest::Unit.new.run(argv)
   end
 end
